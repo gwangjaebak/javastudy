b537401c1b8d4d1164f95bbae6001b16d9aa352d
BIT <BIT@DESKTOP-H078AG4>
2022-01-13 09:16:43 +0900

    practice01 and md

--- practice01/src/main/java/prob2/Prob2.java
+++ practice01/src/main/java/prob2/Prob2.java
@@ -1,7 +1,17 @@
 package prob2;
 
+import java.util.Scanner;
+
 public class Prob2 {
 	public static void main(String[] args) {
+		
 		/* 코드 작성 */
+		
+		for(int i=1; i<=9; i++) {
+			for(int j=i; j<=i+9; j++) {
+				System.out.print(j);
+			}
+			System.out.println();
+		}
 	}
 }
--- practice01/src/main/java/prob3/Prob3.java
+++ practice01/src/main/java/prob3/Prob3.java
@@ -9,6 +9,17 @@ public class Prob3 {
 		Scanner scanner = new Scanner(System.in);
 
 		/* 코드 작성 */
+		System.out.print("숫자를 입력하세요 : ");
+		int num = scanner.nextInt();
+		int sum = 0;
+		
+		for (int i=0; i<=num; i++) {
+			if(i%2 == 1) {
+				sum += i;
+			}
+		}
+		
+		System.out.println(sum);
 		
 		scanner.close();
 	}
