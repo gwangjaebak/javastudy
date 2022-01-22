4d9123099bdc182416baeebbc5fd097f5ae8c4c6
BIT <BIT@DESKTOP-H078AG4>
2022-01-22 17:40:29 +0900

    prac02 04, 05

--- practice02/src/main/java/prob04/Prob04.java
+++ practice02/src/main/java/prob04/Prob04.java
@@ -1,4 +1,7 @@
 package prob04;
+
+import java.util.Arrays;
+
 public class Prob04 {
 
 	public static void main(String[] args) {
@@ -11,11 +14,19 @@ public class Prob04 {
 	
 	public static char[] reverse(String str) {
 		/* 코드를 완성합니다 */
+		String[] arr = new String[str.length()];
+		System.out.println(Arrays.toString(arr));
+		
+//		for (int i=arr.length-1; i<=0; i--) {
+//			arr[i] = String.valueOf(str.charAt(i));
+//		} 
+		
 		return null;
 	}
 
 	public static void printCharArray(char[] array){
 		/* 코드를 완성합니다 */
-		System.out.println( array );
+		System.out.print(array);
+		
 	}
 }
